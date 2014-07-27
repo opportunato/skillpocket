e213966a1a1b9259a46287c5a6650a262e8967ac
opportunato <ryu.gordeyev@gmail.com>
2014-07-27 21:12:36 +0200

    Added simple authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :authenticate
+
+protected
+
+  def authenticate
+    if Rails.env.development?
+      authenticate_or_request_with_http_basic do |u, p| 
+        u == ENV['USERNAME'] && Digest::SHA1.hexdigest(p) == ENV['PASSWORD_HASH']
+      end
+    end
+  end
 end
