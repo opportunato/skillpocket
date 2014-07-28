581efcf11767d9bd93880c70f38f79fca3a67fca
panddr <panddr@gmail.com>
2014-07-28 13:59:13 +0200

    design is ready waiting for copy

--- vendor/assets/javascripts/main.js
+++ vendor/assets/javascripts/main.js
@@ -1,6 +1,20 @@
 $(function() {
 
-var carousel = new Carousel(".carousel");
+var carousel = new Carousel(".carousel"),
+	$ul = $('.carousel ul');
+
+setTimeout(function() {
+	return $("#typed").typed({
+		strings: ["Meet experts near you"],
+		typeSpeed: 20
+	}, 300);
+});
+
 carousel.init();
+
+$ul.on("mousemove", function(e) {
+	e.preventDefault();
+	$ul.removeClass("animation-start");
+});
 
 });
