20d4344e76eea58e99ae1c7d8f19b52df563155b
opportunato <ryu.gordeyev@gmail.com>
2014-07-28 15:43:12 +0200

    Added form for adding experts

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,33 @@
 class LandingController < ApplicationController
   def index
   end
+
+  def poll
+  end
+
+  def poll_submit
+    data = {
+      expert: expert_params,
+      token: ENV['API_TOKEN']
+    }
+
+    url = URI.parse(ENV['POLL_LINK'])
+    http = Net::HTTP.new(url.host, url.port)
+
+    request = Net::HTTP::Post.new(url.path, {'Content-Type' =>'application/json'})
+    request.body = data.to_json
+
+    response = http.request(request)
+
+    redirect_to action: :success
+  end
+
+  def success
+  end
+
+private
+
+  def expert_params
+    params.require(:expert).permit(:twitter_link, :linkedin_link, :site_link, :text_title, :tags, :price)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'landing#index'
+
+  get "/poll", to: "landing#poll"
+  get "/success", to: "landing#success"
+  post "/poll_submit", to: "landing#poll_submit"
 end
