c74e06f319bc044c04c0aa25c6bd44dc61e0be72
panddr <panddr@gmail.com>
2014-07-30 12:46:22 +0200

    form design is ready

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -9,7 +9,7 @@ class LandingController < ApplicationController
   def poll_submit
     step = params[:page].to_i
 
-    if [1, 2, 3, 4].include?(step)
+    if [1, 2, 3].include?(step)
       if step == 1
         token = SecureRandom.uuid
 
@@ -38,25 +38,14 @@ class LandingController < ApplicationController
         }
       elsif step == 3
         @temp_user = TempUser.find_by(token: cookies[:token])
-
-        cookies[:poll_page] = 4
-
-        data = {
-          token: cookies[:token],
-          expert: expert_params_3,
-          step: 3,
-          api_token: ENV['API_TOKEN']
-        }
-      elsif step == 4
-        @temp_user = TempUser.find_by(token: cookies[:token])
         # if params[:image]
         #   temp_image = TempImage.create(image: params[:image])
         # end
 
         data = {
           token: cookies[:token],
-          expert: expert_params_4,
-          step: 4,
+          expert: expert_params_3,
+          step: 3,
           api_token: ENV['API_TOKEN']
         }
 
@@ -72,7 +61,7 @@ class LandingController < ApplicationController
 
       # response = http.request(request)
 
-      if step == 4
+      if step == 3
         redirect_to action: :success
       else
         render json: { success: true }
@@ -94,10 +83,6 @@ private
   end
 
   def expert_params_3
-    params.require(:expert).permit(:skill_title, :skill_description)
-  end
-
-  def expert_params_4
-    params.require(:expert).permit(:price, :tags)
+    params.require(:expert).permit(:skill_title, :skill_description, :price, :tags)
   end
 end
