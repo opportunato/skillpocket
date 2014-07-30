5998954d786723b70602a38fb235d7374c8f0716
opportunato <ryu.gordeyev@gmail.com>
2014-07-30 16:42:19 +0200

    Removed temp images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140730133729) do
+ActiveRecord::Schema.define(version: 20140730144114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,13 +37,6 @@ ActiveRecord::Schema.define(version: 20140730133729) do
     t.boolean  "is_read",    default: false
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "temp_images", force: true do |t|
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
   end
 
 end
