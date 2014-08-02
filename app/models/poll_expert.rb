50d2a1943a9480a25256f3a08d954df5d854c5ca
opportunato <ryu.gordeyev@gmail.com>
2014-08-02 14:58:45 +0200

    Added filters for published and not published

--- app/models/poll_expert.rb
+++ app/models/poll_expert.rb
@@ -2,4 +2,6 @@ class PollExpert < ActiveRecord::Base
   mount_uploader :image, PollExpertUploader
 
   scope :by_step, -> (step) { where(step: step) }
+  scope :published, -> { where(expert_created: true) }
+  scope :notpublished, -> { where(expert_created: false) }
 end
