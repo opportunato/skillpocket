8b2b88d2c5d8e99c1caa83829971d59242b3ce75
panddr <panddr@gmail.com>
2014-08-04 20:36:57 +0200

    arrows keyboard added

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -7,8 +7,20 @@ $(function() {
 				$fullHeight = $('.full-height'),
 				$b = $('body'),
 				$w = $(window),
-				windowHeight = $w.height();
+				windowHeight = $w.height(),
+				nexthammertime,
+				previoushammertime,
+				current_pane = 0,
+				previousPost = $('.previous'),
+				nextPost = $('.next'),
+				panes = $('.carousel li.pane'),
+				pane_count = panes.length,
+				nextCarousel,
+				previousCarousel;
 
+
+
+//typed
 		setTimeout(function() {
 			return $("#typed").typed({
 				strings: ["Meet experts near you"],
@@ -16,8 +28,9 @@ $(function() {
 			}, 300);
 		});
 
-		carousel.init();
 
+
+//ui
 		$('a.button').on("click", function(e) {
 			e.preventDefault();
 		});
@@ -40,5 +53,57 @@ $(function() {
 		});
 
 		$w.trigger("resize");
+
+
+
+
+
+//carousel
+		carousel.init();
+
+		nextCarousel = function() {
+			var nextCarousel;
+			nextCarousel = new Carousel(".carousel");
+			nextCarousel.init();
+			nextCarousel.showPane(current_pane + 1, true);
+			if (current_pane < pane_count - 1) {
+				current_pane += 1;
+			}
+		}
+
+		previousCarousel = function() {
+
+			var previousCarousel;
+			previousCarousel = new Carousel(".carousel");
+			previousCarousel.init();
+			previousCarousel.showPane(current_pane - 1, true);
+			if (current_pane > 0) {
+				current_pane -= 1;
+			}
+
+		}
+
+		nexthammertime = Hammer(nextPost[0]).on("tap", function(event) {
+			event.preventDefault();
+			nextCarousel();
+		});
+
+		previoushammertime = Hammer(previousPost[0]).on("tap", function(event) {
+			event.preventDefault();
+			previousCarousel();
+		});
+
+		$b.keydown(function(e) {
+
+			if(e.keyCode == 37) { // left
+
+				previousCarousel();
+			}
+
+			else if(e.keyCode == 39) { // right
+
+				nextCarousel();
+			}
+		});
 	}
 });
