a2e10d6f9c46406cc31f2e2934962d7d5d4546db
opportunato <ryu.gordeyev@gmail.com>
2014-10-22 13:27:02 +0400

    Added admin authentication for the whole site

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,4 @@
 class ApiController < ApplicationController
   skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_admin
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_admin
   before_action :authenticate!
 
   helper_method :signed_in?, :current_user
@@ -18,5 +19,14 @@ class ApplicationController < ActionController::Base
 
   def warden
     request.env['warden']
+  end
+
+protected
+  def authenticate_admin
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |u, p| 
+        u == ENV['USERNAME'] && Digest::SHA1.hexdigest(p) == ENV['PASSWORD_HASH']
+      end
+    end
   end
 end
