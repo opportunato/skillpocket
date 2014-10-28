070a0380365b89b5b5d1fd7998c89a3f7dc3dc6d
opportunato <ryu.gordeyev@gmail.com>
2014-10-28 18:20:00 +0300

    Fixed typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.asset_host = "http://shillpocket.com/"
+  config.action_mailer.asset_host = "http://skillpocket.com/"
   config.action_mailer.default_url_options = {
     host: "skillpocket.com"
   }
