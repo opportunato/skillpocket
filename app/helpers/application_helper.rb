6177e736c688760160e63d2b67ddadf365a3eee9
opportunato <ryu.gordeyev@gmail.com>
2014-10-28 22:52:35 +0300

    Added SEO

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def expert_title(expert)
+    "#{expert.full_name} - #{expert.skill_title}"
+  end  
+
+  def full_url(path)
+    request.protocol + request.host + path
+  end
 end
