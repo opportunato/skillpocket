fd256269789bc9bf0e673ee12bc41247d26697f5
Phil Pirozhkov <pirjsuka@gmail.com>
2014-11-05 18:42:44 +0300

    simplify expert specs

--- spec/requests/api/v1/experts_spec.rb
+++ spec/requests/api/v1/experts_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::ExpertsController do
+  let(:user) { create :user }
+
   before do
     @first_expert = create :skilled_user
     @second_expert = create :category_tag_skilled_user
@@ -14,7 +16,7 @@ RSpec.describe Api::V1::ExpertsController do
     @second_expert
   end
 
-  it 'returns experts' do
+  it 'returns experts in correct format' do
     get "/api/v1/experts/"
 
     # it_behaves_like 'token protected resource'
@@ -42,7 +44,7 @@ RSpec.describe Api::V1::ExpertsController do
         }],
         'twitter_url' => first_expert.twitter_url,
         'website_url' => first_expert.website_url
-      }, 
+      },
       {
         'id' => second_expert.id,
         'email' => second_expert.email,
@@ -79,67 +81,16 @@ RSpec.describe Api::V1::ExpertsController do
     it "returns only experts with that category" do
       get "/api/v1/experts?category=#{URI.encode(second_expert.skill.tags.last.name)}"
 
-      expect(response_json).to eq([
-        {
-          'id' => second_expert.id,
-          'email' => second_expert.email,
-          'about' => second_expert.about,
-          'behance_url' => second_expert.behance_url,
-          'github_url' => second_expert.github_url,
-          'full_name' => second_expert.full_name,
-          'job' => second_expert.job,
-          'linkedin_url' => second_expert.linkedin_url,
-          'photo' => second_expert.photo.url(:small),
-          'price' => second_expert.price,
-          'profile_banner_url' => second_expert.profile_banner.url(:normal),
-          'skill_title' => second_expert.skill.title,
-          'slug' => second_expert.slug,
-          'stackoverflow_url' => second_expert.stackoverflow_url,
-          'categories' => [{
-            'id' => second_expert.skill.tags.last.id,
-            'name' => second_expert.skill.tags.last.name
-          }],
-          'tags' => [{
-            'id' => second_expert.skill.tags.first.id,
-            'name' => second_expert.skill.tags.first.name
-          }, {
-            'id' => second_expert.skill.tags.last.id,
-            'name' => second_expert.skill.tags.last.name
-          }],
-          'twitter_url' => second_expert.twitter_url,
-          'website_url' => second_expert.website_url
-        }
-      ])
+      expect(response_json.size).to eq 1
+      expect(response_json.first['id']).to eq second_expert.id
     end
   end
 
-  it 'returns expert' do
-    get "/api/v1/experts/#{first_expert.slug}"
+  context 'when given a slug' do
+    it 'returns expert' do
+      get "/api/v1/experts/#{first_expert.slug}"
 
-    expect(response_json).to eq(
-      {
-        'id' => first_expert.id,
-        'email' => first_expert.email,
-        'about' => first_expert.about,
-        'behance_url' => first_expert.behance_url,
-        'github_url' => first_expert.github_url,
-        'full_name' => first_expert.full_name,
-        'job' => first_expert.job,
-        'linkedin_url' => first_expert.linkedin_url,
-        'photo' => first_expert.photo.url(:small),
-        'price' => first_expert.price,
-        'profile_banner_url' => first_expert.profile_banner.url(:normal),
-        'skill_title' => first_expert.skill.title,
-        'slug' => first_expert.slug,
-        'stackoverflow_url' => first_expert.stackoverflow_url,
-        'categories' => [],
-        'tags' => [{
-          'id' => first_expert.skill.tags.first.id,
-          'name' => first_expert.skill.tags.first.name,
-        }],
-        'twitter_url' => first_expert.twitter_url,
-        'website_url' => first_expert.website_url
-      }
-    )
+      expect(response_json['id']).to eq first_expert.id
+    end
   end
 end
