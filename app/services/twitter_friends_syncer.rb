8ee74c7f6ed35bb33a6fb6e0d5635f5c52573aa6
opportunato <ryu.gordeyev@gmail.com>
2014-11-12 22:33:31 +0300

    Added proper error catching for twitter sync

--- app/services/twitter_friends_syncer.rb
+++ app/services/twitter_friends_syncer.rb
@@ -16,21 +16,26 @@ class TwitterFriendsSyncer
     expert_twitter_ids.clear
     expert_twitter_ids.merge(*expert_ids)
 
-    begin
+    
       experts.each do |expert|
-        twitter_talker = TwitterTalker.new(expert.twitter_token, expert.twitter_token_secret)
-      
-        follower_ids = twitter_talker.follower_ids(user_id: expert.twitter_id)
-        expert.twitter_followers.clear
-        if follower_ids.length > 0
-          expert.twitter_followers.merge(*follower_ids)
+        begin
+          twitter_talker = TwitterTalker.new(expert.twitter_token, expert.twitter_token_secret)
+        
+          if twitter_talker.user.followers_count <= 75000
+            follower_ids = twitter_talker.follower_ids(user_id: expert.twitter_id)
+            expert.twitter_followers.clear
+            if follower_ids.length > 0
+              expert.twitter_followers.merge(*follower_ids)
+            end
+          else
+            puts "User '#{expert.twitter_handle}' has too many followers."
+          end
+        rescue Twitter::Error::Unauthorized
+          puts "User '#{expert.twitter_handle}' is unauthorized."
+        rescue Twitter::Error::TooManyRequests
+          puts "User '#{expert.twitter_handle}' exceeded request limit."
         end
       end
-    rescue Twitter::Error::Unauthorized
-      puts "User '#{user.twitter_handle}' is unauthorized."
-    rescue Twitter::Error::TooManyRequests
-      puts "User '#{user.twitter_handle}' exceeded request limit."
-    end
 
     @users.each do |user|
       begin
@@ -75,13 +80,17 @@ class TwitterFriendsSyncer
 private
 
   def update_twitter_friend_ids(user)
-    twitter_talker = TwitterTalker.new(user.twitter_token, user.twitter_token_secret)
+    if twitter_talker.user.friends_count <= 75000
+      twitter_talker = TwitterTalker.new(user.twitter_token, user.twitter_token_secret)
 
-    friend_ids = twitter_talker.friend_ids(user_id: user.twitter_id)
-    user.twitter_friends.clear
+      friend_ids = twitter_talker.friend_ids(user_id: user.twitter_id)
+      user.twitter_friends.clear
 
-    if friend_ids.length > 0
-      user.twitter_friends.merge(*friend_ids)
+      if friend_ids.length > 0
+        user.twitter_friends.merge(*friend_ids)
+      end
+    else
+      puts "User '#{user.twitter_handle}' has too many friends."
     end
   end
 end
