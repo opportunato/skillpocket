5f256690dd850e9f73673a17a458b9d3b60645e1
opportunato <ryu.gordeyev@gmail.com>
2014-11-19 16:59:55 +0300

    Temporarily removed mutual_followers calculation

--- app/services/twitter_friends_syncer.rb
+++ app/services/twitter_friends_syncer.rb
@@ -15,29 +15,28 @@ class TwitterFriendsSyncer
 
     expert_twitter_ids.clear
     expert_twitter_ids.merge(*expert_ids)
-
     
-    experts.each do |expert|
-      begin
-        twitter_talker = TwitterTalker.new(expert.twitter_token, expert.twitter_token_secret)
+    # experts.each do |expert|
+    #   begin
+    #     twitter_talker = TwitterTalker.new(expert.twitter_token, expert.twitter_token_secret)
       
-        if twitter_talker.user.followers_count <= 75000
-          follower_ids = twitter_talker.follower_ids(user_id: expert.twitter_id)
-          expert.twitter_followers.clear
-          if follower_ids.length > 0
-            expert.twitter_followers.merge(*follower_ids)
-          end
-        else
-          puts "User '#{expert.twitter_handle}' has too many followers."
-        end
-      rescue Twitter::Error::Unauthorized
-        puts "User '#{expert.twitter_handle}' is unauthorized."
-      rescue Twitter::Error::TooManyRequests
-        puts "User '#{expert.twitter_handle}' exceeded request limit."
-      rescue Twitter::Error
-        puts "Unexplained Error on user #{expert.twitter_handle}'"
-      end
-    end
+    #     if twitter_talker.user.followers_count <= 75000
+    #       follower_ids = twitter_talker.follower_ids(user_id: expert.twitter_id)
+    #       expert.twitter_followers.clear
+    #       if follower_ids.length > 0
+    #         expert.twitter_followers.merge(*follower_ids)
+    #       end
+    #     else
+    #       puts "User '#{expert.twitter_handle}' has too many followers."
+    #     end
+    #   rescue Twitter::Error::Unauthorized
+    #     puts "User '#{expert.twitter_handle}' is unauthorized."
+    #   rescue Twitter::Error::TooManyRequests
+    #     puts "User '#{expert.twitter_handle}' exceeded request limit."
+    #   rescue Twitter::Error
+    #     puts "Unexplained Error on user #{expert.twitter_handle}'"
+    #   end
+    # end
 
     @users.each do |user|
       begin
@@ -58,29 +57,29 @@ class TwitterFriendsSyncer
           end
         end
 
-        user_friended_expert_follower_inserts = []
+        # user_friended_expert_follower_inserts = []
 
-        experts.each do |expert|
-          if user.id != expert.id
-            user_friended_expert_followers = expert.twitter_followers & user.twitter_friends
-            expert_id = expert.id
+        # experts.each do |expert|
+        #   if user.id != expert.id
+        #     user_friended_expert_followers = expert.twitter_followers & user.twitter_friends
+        #     expert_id = expert.id
 
-            user_friended_expert_follower_inserts.concat(user_friended_expert_followers.map do |twitter_id|
-                expert_id = User.sanitize(expert_id)
-                twitter_id = User.sanitize(twitter_id)
+        #     user_friended_expert_follower_inserts.concat(user_friended_expert_followers.map do |twitter_id|
+        #         expert_id = User.sanitize(expert_id)
+        #         twitter_id = User.sanitize(twitter_id)
 
-                "(#{user_id}, #{expert_id}, #{twitter_id})"
-              end
-            )
-          end
-        end
+        #         "(#{user_id}, #{expert_id}, #{twitter_id})"
+        #       end
+        #     )
+        #   end
+        # end
 
-        ActiveRecord::Base.transaction do
-          ActiveRecord::Base.connection.execute("DELETE FROM user_friended_expert_followers WHERE user_id = #{user_id}")
-          if user_friended_expert_follower_inserts.length > 0
-            ActiveRecord::Base.connection.execute("INSERT INTO user_friended_expert_followers (user_id, expert_id, twitter_id) VALUES #{user_friended_expert_follower_inserts.join(', ')};")
-          end
-        end
+        # ActiveRecord::Base.transaction do
+        #   ActiveRecord::Base.connection.execute("DELETE FROM user_friended_expert_followers WHERE user_id = #{user_id}")
+        #   if user_friended_expert_follower_inserts.length > 0
+        #     ActiveRecord::Base.connection.execute("INSERT INTO user_friended_expert_followers (user_id, expert_id, twitter_id) VALUES #{user_friended_expert_follower_inserts.join(', ')};")
+        #   end
+        # end
       rescue Twitter::Error::Unauthorized
         puts "User '#{user.twitter_handle}' is unauthorized."
       rescue Twitter::Error::TooManyRequests
